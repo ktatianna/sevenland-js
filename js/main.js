2c3a7fdba85c827835680cc8a67fb5e5d2cb094c
tati <ktgomezgaitan@gmail.com>
2022-01-13 20:53:52 -0500

    view product detail

--- js/main.js
+++ js/main.js
@@ -1,59 +1,8 @@
+//variables para el DOM
 const main = document.getElementById('items');
 
-// Productos
-const products = [
-    {
-        id: 1,
-        nombre: "Kalanchoe",
-        precio: 10000,
-        imagen: "producto1.PNG",
-        descripcion:
-            "El kalanchoe es, sin duda, una planta idónea para la decoración de espacios interiores y exteriores. Con colores exóticos como el rojo, fucsia, naranja y amarillo, y unas hojas carnosas que le ayudan a dosificar el agua.",
-    },
-    {
-        id: 2,
-        nombre: "Cactus",
-        precio: 12000,
-        imagen: "producto2.PNG",
-        descripcion:
-            "Debido a que están preparados naturalmente para almacenar agua en su estructura de tallos robustos y espinosos, los cactus se han convertido en el ornamento predilecto para espacios exteriores e interiores.",
-    },
-    {
-        id: 3,
-        nombre: "Crassula Morgan",
-        precio: 20100,
-        imagen: "producto3.PNG",
-        descripcion:
-            "Coloca esta crassula en un lugar donde reciba mucha luz durante todo el año y, si puede ser, en el que circule aire. ¡Debe resguardarse del frío extremo! Poco riego. Esta planta no necesita mucha agua.",
-    },
-    {
-        id: 4,
-        nombre: "Richodiadema Densum",
-        precio: 6000,
-        imagen: "producto4.PNG",
-        descripcion:
-            "Es una planta de crecimiento lento. Desarrolla tallos rastreros, irregulares y leñosos no excesivamente largos muy ramificados desde la base formando masas vegetales muy tupidas de poca altura, de cinco a diez centímetros.",
-    },
-    {
-        id: 5,
-        nombre: "Carnivora",
-        precio: 18000,
-        imagen: "producto5.PNG",
-        descripcion:
-            "Obtiene parte o la mayoría de sus necesidades nutricionales mediante la captura y el consumo de protozoos y animales, especialmente insectos .",
-    },
-    {
-        id: 6,
-        nombre: "Astrophytum Myriostigmas",
-        precio: 11000,
-        imagen: "producto6.PNG",
-        descripcion:
-            "Planta de color verde brillante, sin espinas, por lo general solitaria, suelen tener cinco costillas, aunque pueden variar de 3 a 10 y su epidermis esta cubierta de puntos de color blanco grisáceo.",
-    },
-];
-
 //Carrito de productos 
-let cart = []; 
+let cart = [];
 
 //Formato precios 
 const formatter = new Intl.NumberFormat('en-US', {
@@ -62,37 +11,42 @@ const formatter = new Intl.NumberFormat('en-US', {
     minimumFractionDigits: 0
 })
 
-
 //añadir productos al carrito de compras
-function shoppingCart(id) { 
+function shoppingCart(id) {
     //añadir elementos al carrito
-    const resultado = products.find( product => product.id == id );
+    const resultado = products.find(product => product.id == id);
     cart.push(resultado);
-    console.log(cart); 
+    console.log(cart);
 
     //calcular total del carrito
     const precioTotal = cart.map(product => product.precio).reduce((prev, curr) => prev + curr, 0);
-    console.log("Total: " +precioTotal);
+    console.log("Total: " + precioTotal);
 }
 
-function showProducts() {
+
+
+function ProductDetail(i) {
+    location.href=`src/views/productDetail.html?id=${i}`;  
+}
+
+window.onload = function showProducts() {
     main.innerHTML = '';
     products.forEach(product => {
         const productsHTML = document.createElement('div');
         productsHTML.innerHTML = `
             <div class="col">
-                <div id="${product.id}" class="card h-100 text-center">
-                <img src="src/multimedia/img/productos/${product.imagen}" class="card-img-top" alt="...">
-                <div class="card-body">
-                    <h5 class="card-title">${product.nombre}</h5>
-                    <p class="card-price">${formatter.format(product.precio)} c/u </p>
-                    <button onclick="shoppingCart(${product.id})" href="#" class="btn btn-card">Añadir al carrito</button>
-                </div>
+                <div id="${product.id}" class="card card-products h-100 text-center">
+                    <img onclick="ProductDetail(${product.id})" src="src/multimedia/img/productos/${product.imagen}" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">${product.nombre}</h5>
+                        <p class="card-price">${formatter.format(product.precio)} c/u </p>
+                        <button onclick="shoppingCart(${product.id})" href="#" class="btn btn-card">Añadir al carrito</button>
+                    </div>
                 </div>
             </div> 
             `
-            main.appendChild(productsHTML);
+        main.appendChild(productsHTML);
     })
 }
 
-showProducts();
+
---
+++ js/productsList.js
@@ -0,0 +1,75 @@
+// Productos
+const products = [
+    {
+        id: 1,
+        nombre: "Kalanchoe",
+        precio: 10000,
+        imagen: "producto1.PNG",
+        descripcion:
+            "El kalanchoe es, sin duda, una planta idónea para la decoración de espacios interiores y exteriores. Con colores exóticos como el rojo, fucsia, naranja y amarillo, y unas hojas carnosas que le ayudan a dosificar el agua.",
+    },
+    {
+        id: 2,
+        nombre: "Cactus",
+        precio: 12000,
+        imagen: "producto2.PNG",
+        descripcion:
+            "Debido a que están preparados naturalmente para almacenar agua en su estructura de tallos robustos y espinosos, los cactus se han convertido en el ornamento predilecto para espacios exteriores e interiores.",
+    },
+    {
+        id: 3,
+        nombre: "Crassula Morgan",
+        precio: 20100,
+        imagen: "producto3.PNG",
+        descripcion:
+            "Coloca esta crassula en un lugar donde reciba mucha luz durante todo el año y, si puede ser, en el que circule aire. ¡Debe resguardarse del frío extremo! Poco riego. Esta planta no necesita mucha agua.",
+    },
+    {
+        id: 4,
+        nombre: "Richodiadema Densum",
+        precio: 6000,
+        imagen: "producto4.PNG",
+        descripcion:
+            "Es una planta de crecimiento lento. Desarrolla tallos rastreros, irregulares y leñosos no excesivamente largos muy ramificados desde la base formando masas vegetales muy tupidas de poca altura, de cinco a diez centímetros.",
+    },
+    {
+        id: 5,
+        nombre: "Carnivora",
+        precio: 18000,
+        imagen: "producto5.PNG",
+        descripcion:
+            "Obtiene parte o la mayoría de sus necesidades nutricionales mediante la captura y el consumo de protozoos y animales, especialmente insectos .",
+    },
+    {
+        id: 6,
+        nombre: "Astrophytum Myriostigmas",
+        precio: 11000,
+        imagen: "producto6.PNG",
+        descripcion:
+            "Planta de color verde brillante, sin espinas, por lo general solitaria, suelen tener cinco costillas, aunque pueden variar de 3 a 10 y su epidermis esta cubierta de puntos de color blanco grisáceo.",
+    },
+    {
+        id: 7,
+        nombre: "Terrario Dodecaedro.",
+        precio: 30000,
+        imagen: "producto7.PNG",
+        descripcion:
+            "Tenemos tres estilos de vidrio: triangular, cúbico y dodecaedro. Cada uno se puede fabricar con cactus o suculentas.",
+    },
+    {
+        id: 8,
+        nombre: "Cactus viejito",
+        precio: 50000,
+        imagen: "producto8.PNG",
+        descripcion:
+            "El Cactus cabeza de viejo es una especie muy llamativa, nombrada así debido a los largos cabellos blancos que le dan la apariencia de una persona de edad",
+    },
+    {
+        id: 9,
+        nombre: "Aizoaceae",
+        precio: 35000,
+        imagen: "producto9.PNG",
+        descripcion:
+            "Estas plantas son originarias de zonas desérticas del continente africano. Por lo tanto tienen la capacidad de almacenar agua al igual que las suculentas para poder sobrevivir durante los fuertes veranos.",
+    },
+];
